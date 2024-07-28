150c6e328d0e3f2feacb6f1136a7bace3a358d03
unknown <jithinbp007@gmail.com>
2024-07-28 09:42:21 +0530

    styling section one

--- src/components/PriceComponent.jsx
+++ src/components/PriceComponent.jsx
@@ -2,23 +2,34 @@ import styled from "@emotion/styled";
 import Button from "./common/Button";
 
 const Container = styled.div`
-  padding: 1rem 2rem;
+  /* padding: 1rem 2rem; */
+  max-width: 40rem;
+  border-radius: 7px;
+  overflow: hidden;
 `;
 
-const SectionOne = styled.div``;
+const SectionOne = styled.div`
+  background-color: hsl(var(--clr-white));
+  padding-inline: 2.5rem;
+  padding-block: 2rem;
+`;
 
 const PricingSection = styled.section``;
 
 const About = styled.div``;
 
 const Title = styled.span`
+  margin-bottom: 1rem;
   display: block;
-  font-size: 1.125rem;
+  font-size: 1.5rem;
   font-weight: 700;
   color: hsl(var(--clr-primary-cyan-200));
 `;
 
 const SubTitle = styled.span`
+  display: block;
+  margin-bottom: 0.5rem;
+  font-size: 1.125rem;
   font-weight: 700;
   color: hsl(var(--clr-primary-yellow));
 `;
